8e911f96dbeb123360071f2c333a4194872e6b59
Mario.Ametrano <wario.ametrano@hotmail.com>
2026-05-26 12:10:03 +0200

    Enhance test-file-2.spec.js with execution comments
    Added comments to explain authentication test execution and worker usage.

--- Smoke Tests/Multiple_Authentication_Test/tests/test-file-2.spec.js
+++ Smoke Tests/Multiple_Authentication_Test/tests/test-file-2.spec.js
@@ -18,3 +18,9 @@ test("auth - TF #2 - TC #2", async({page,browserName}) => {
     await page.waitForTimeout(timeout);
     await page.screenshot({path:`screenshots/TF2-TC2-${browserName}.png`});
 });
+
+// comando per eseguire solo il test di autenticazione in questo file: npx playwright test test-file-1.spec.js --headed --workers 1
+
+/* I worker sono “mini-motori paralleli” che Playwright usa per eseguire i test in parallelo più velocemente e in modo isolato.
+più test in parallelo hai più worker devi aggiungere così vengono divisi i processi della CPU per ogni worker 
+quindi nel comando devi specificare quanti usarne se hai 1000 test ne puoi usare anche 6 in base alla potenza del pc quanti test in contemporanea sopporta*/
